2fe0ebfd35c689def35a332124e50239886d9367
Showaa-06 <shogoshogo0611@icloud.com>
2024-12-04 00:31:46 +0900

    flex

--- resources/views/appointment/form.blade.php
+++ resources/views/appointment/form.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    <title>物件予約フォーム</title>
+    <title>物件内見予約フォーム</title>
     
 </head>
 <body>  
