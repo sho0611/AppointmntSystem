731bde9a73325d8c72be440e4e1d148f8c0ab2dc
Showaa-06 <shogoshogo0611@icloud.com>
2024-12-13 01:14:47 +0900

    TypeScript unionType

--- typescript-lesson/index.js
+++
@@ -1,2 +0,0 @@
-var hello = 'Hello, TypeScript!';
-console.log(hello);
--- typescript-lesson/index.ts
+++ typescript-lesson/index.ts
@@ -1,8 +1,39 @@
-import axios from "axios";
-let hello: string = 'Hello, TypeScript!';   
-console.log(hello);  
-console.log();  
+import { LazyResult } from "postcss";
 
-//ドキュメントとしての側面を利用するため
-//Linterとしての側面を利用するため  
-//ES%へのコンパイラとしての側面を利用するため
+let hasValue = true;
+let count = 10;
+let float = 3.14;
+let negative = -0.12;
+let single = 'hello';
+let double = "hello";
+
+const person:{
+    name: string;  
+    age: number;
+} = {
+    name: 'Jack',
+    age: 21 
+}
+
+const fuits: string[] = ['Apple', 'Banana', 'Grape']; 
+const book: [string, number, boolean] = ['business', 1500, false];  
+
+enum CofeeSize {
+    SHORT = 'SHORT',
+    TALL = 'TALL',
+    GRANDE = 'GRANDE',
+    VENTI = 'VENTI' 
+}
+
+const coffee = {
+    hot: true,
+    size: CofeeSize.TALL
+}
+
+coffee.size = CofeeSize.SHORT;
+console.log(coffee.size);
+
+let unionType: number | string = 10;
+unionType = 'hello';
+
+let unionTypes: (number | string)[] = [21, 'hello'];
